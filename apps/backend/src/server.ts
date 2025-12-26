698b7f5a94f00b3ee50a6b0153b5656bb93e558d
Mavihs <shivamc86@gmail.com>
2025-12-26 17:56:19 +0530

    Initial Commit

---
+++ apps/backend/src/server.ts
@@ -0,0 +1,11 @@
+// src/server.ts
+import "dotenv/config";
+import { createApp } from "./app.js";
+
+const PORT = process.env.PORT || 4000;
+
+const app = createApp();
+
+app.listen(PORT, () => {
+  console.log(`Backend running on http://localhost:${PORT}`);
+});
